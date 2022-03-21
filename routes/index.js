8b2cb21b3573a0dee9b22e3bb2df3c45cf848313
KURT ORIOQUE <kurtorioque112@gmail.com>
2022-03-22 01:49:44 +0800

    firt commit

---
+++ routes/index.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const router = express.Router();
+const sha1 = require('sha1')
+const axios = require('axios');
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+router.post('/pay', async (req,res) => {
+  const {amount,txnid,callback_url,name,mobile,description,redirect_url} = req.body.data;
+
+  const sha = await sha1(`${amount}${txnid}08919f7b1e1299f74feb4309ab388d12e6e868ae`)
+  const formData = {
+    amount: amount,
+    txnid: txnid,
+    callback_url: callback_url,
+    digest: sha,
+    redirect_url: 'https://simple-order-64158.web.app/',
+    name,
+    mobile,
+    description,
+    redirect_url
+  };
+  await axios.post('https://pgi-stg-ws.pearlpay.io/api/v1/transactions/generate',formData,{
+    headers:{
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Headers': '*',
+      'Content-Type':'application/json',
+      'X-PearlPay-Token': '08919f7b1e1299f74feb4309ab388d12e6e868ae',
+      'X-PearlPay-Code': 'PEARLPAY_TEST_BILLER'
+    }
+  }).then(response => {
+     const {url} = response.data.data;
+     return res.status(200).send({url:url})
+  }).catch(error => {
+     return res.status(error);
+  });
+})
+
+module.exports = router;
